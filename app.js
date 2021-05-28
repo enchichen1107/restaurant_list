de04566404db20626ea238e3292341e0a0765306
enchichen1107 <b03409032@gmail.com>
2021-05-28 20:26:11 +0800

    Add google map and icons

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
 
-// show restaurant detail
+// show restaurant details
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
   res.render('show', { restaurant: restaurant })

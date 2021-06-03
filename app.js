3998cb996a31cb7ab41b1b311c63905f9e46c6da
enchichen1107 <b03409032@gmail.com>
2021-06-03 18:03:43 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const restaurantList = require('./restaurant.json')
 const Restaurant = require('./models/restaurant')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
@@ -24,15 +25,38 @@ db.once('open', () => {
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-// setting static files
+// setting static files and bodyParser
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-// routes setting
+// show all restaurants
 app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.error(error))
+})
+
+// create new restaurant-show new page
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+// create new restaurant-post data
+app.post('/restaurants', (req, res) => {
+  const newRestaurant = req.body
+  Restaurant.create({
+    name: newRestaurant.name,
+    category: newRestaurant.category,
+    image: newRestaurant.image,
+    location: newRestaurant.location,
+    phone: newRestaurant.phone,
+    google_map: newRestaurant.google_map,
+    rating: newRestaurant.rating,
+    description: newRestaurant.description
+  })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 // show restaurant details

2181344bfab02ec36dfbab231f05489566cf0cfc
enchichen1107 <b03409032@gmail.com>
2021-06-10 18:23:19 +0800

    refactor: delete unused code in app.js

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
 // require packages used in the project
 const express = require('express')
 const exphbs = require('express-handlebars')
-// const mongoose = require('mongoose')
-const Restaurant = require('./models/restaurant')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 require('./config/mongoose')
 const app = express()
 const port = 3000
-
-// connect to mongoDB
-// mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
-// // get db connection status
-// const db = mongoose.connection
-// // connection error
-// db.on('error', () => {
-//   console.log('mongodb error!')
-// })
-// // connection success
-// db.once('open', () => {
-//   console.log('mongodb connected!')
-// })
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -32,95 +17,6 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
-
-// show all restaurants
-// app.get('/', (req, res) => {
-//   Restaurant.find()
-//     .lean()
-//     .then(restaurants => res.render('index', { restaurants }))
-//     .catch(error => console.error(error))
-// })
-
-// // create new restaurant- show new page
-// app.get('/restaurants/new', (req, res) => {
-//   return res.render('new')
-// })
-
-// // create new restaurant- post data
-// app.post('/restaurants', (req, res) => {
-//   const { name, category, image, location, phone, google_map, rating, description } = req.body
-//   Restaurant.create({
-//     name,
-//     category,
-//     image,
-//     location,
-//     phone,
-//     google_map,
-//     rating,
-//     description
-//   })
-//     .then(() => res.redirect('/'))
-//     .catch(error => console.log(error))
-// })
-
-// // show restaurant details
-// app.get('/restaurants/:id', (req, res) => {
-//   const id = req.params.id
-//   return Restaurant.findById(id)
-//     .lean()
-//     .then((restaurant) => res.render('detail', { restaurant }))
-//     .catch(error => console.log(error))
-// })
-
-// // edit restaurant- show edit page
-// app.get('/restaurants/:id/edit', (req, res) => {
-//   const id = req.params.id
-//   return Restaurant.findById(id)
-//     .lean()
-//     .then((restaurant) => res.render('edit', { restaurant }))
-//     .catch(error => console.log(error))
-// })
-
-// // edit restaurant- post data
-// app.put('/restaurants/:id', (req, res) => {
-//   const id = req.params.id
-//   const { name, category, image, location, phone, google_map, rating, description } = req.body
-//   return Restaurant.findById(id)
-//     .then(restaurant => {
-//       restaurant.name = name
-//       restaurant.category = category
-//       restaurant.image = image
-//       restaurant.location = location
-//       restaurant.phone = phone
-//       restaurant.google_map = google_map
-//       restaurant.rating = rating
-//       restaurant.description = description
-//       return restaurant.save()
-//     })
-//     .then(() => res.redirect(`/restaurants/${id}`))
-//     .catch(error => console.log(error))
-// })
-
-// // delete restaurant
-// app.delete('/restaurants/:id', (req, res) => {
-//   const id = req.params.id
-//   return Restaurant.findById(id)
-//     .then(restaurant => restaurant.remove())
-//     .then(() => res.redirect('/'))
-//     .catch(error => console.log(error))
-// })
-
-// // search restaurants
-// app.get('/restaurants', (req, res) => {
-//   const keyword = req.query.keyword.trim()
-//   const regex = new RegExp(keyword, 'i')
-//   Restaurant.find({ $or: [{ name: { $regex: regex } }, { category: { $regex: regex } }] })
-//     .lean()
-//     .then(restaurants => {
-//       res.render('index', { restaurants, keyword })
-//     })
-//     .catch(error => console.log(error))
-// })
 
 // start and listen on the Express server
 app.listen(port, () => {

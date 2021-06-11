86ef14b10d998fc383aca5f44b900565f71f03f7
enchichen1107 <b03409032@gmail.com>
2021-06-11 17:15:42 +0800

    refactor: remove unused variables

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const helpers = require('handlebars-helpers')()
 const routes = require('./routes')
 require('./config/mongoose')
+require('handlebars-helpers')()
 const app = express()
 const port = 3000
 

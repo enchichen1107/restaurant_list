97769d6c56dbd441ae345dc14d81fe3c90bedadf
enchichen1107 <b03409032@gmail.com>
2021-06-12 10:07:27 +0800

    refactor: modify comments

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 // add Express and Express router
 const express = require('express')
 const router = express.Router()
+
 // add router modules
 // add home module
 const home = require('./modules/home')
+
 // if request url matches '/', direct to home module
 router.use('/', home)
+
 // add restaurants module
 const restaurants = require('./modules/restaurants')
+
 // if request url matches '/restaurants' , direct to restaurant module
 router.use('/restaurants', restaurants)
+
 // export router
 module.exports = router
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,10 @@
 // add Express and Express router
 const express = require('express')
 const router = express.Router()
+
 // add Restaurant model
 const Restaurant = require('../../models/restaurant')
+
 // define home page router
 router.get('/', (req, res) => {
   Restaurant.find()
@@ -10,5 +12,6 @@ router.get('/', (req, res) => {
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.error(error))
 })
+
 // export router
 module.exports = router

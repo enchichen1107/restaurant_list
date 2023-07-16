83422a0a5adcefda3b1032c5837e88b220161cee
enchichen1107 <b03409032@gmail.com>
2023-07-16 22:47:33 +0800

    feat: modify login and register detail

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -26,56 +26,48 @@ router.post('/register', (req, res) => {
   // get register form params
   const { name, email, password, confirmPassword } = req.body
   const errors = []
-  if (!name || !email || !password || !confirmPassword) {
-    errors.push({ message: '所有欄位都是必填。' })
+  if (!email || !password || !confirmPassword) {
+    errors.push({ message: 'Email 與 密碼 是必填。' })
   }
   if (password !== confirmPassword) {
     errors.push({ message: '密碼與確認密碼不相符！' })
   }
-  if (errors.length) {
-    return res.render('register', {
-      errors,
-      name,
-      email,
-      password,
-      confirmPassword
-    })
-  }
-  // check whether user is registered
-  User.findOne({ email }).then(user => {
-    // if registered back to login page
-    if (user) {
-      errors.push({ message: '這個 Email 已經註冊過了。' })
-      //console.log('User already exists.')
-      res.render('register', {
-        errors,
-        name,
-        email,
-        password,
-        confirmPassword
-      })
-    } else {
-      // if haven't registered, create user
-      return bcrypt
-        .genSalt(10)
-        .then(salt => bcrypt.hash(password, salt))
-        .then(hash => User.create({
+  // check whether registered
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        errors.push({ message: '此 Email 已經註冊過了。' })
+      }
+
+      if (errors.length) {
+        return res.render('register', {
+          errors,
           name,
           email,
-          password: hash
-        }))
-        .then(() => req.flash('success_msg', '你已成功註冊，請重新登入。'))
-        .then(() => res.redirect('/users/login'))
-        .catch(err => console.log(err))
-    }
-  })
+          password,
+          confirmPassword
+        })
+      } else {
+        return bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(password, salt))
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          }))
+          .then(() => req.flash('success_msg', '已成功註冊，請重新登入。'))
+          .then(() => res.redirect('/users/login'))
+          .catch(err => console.log(err))
+      }
+    })
     .catch(err => console.log(err))
 })
 
 //show logout
 router.get('/logout', (req, res) => {
   req.logout()
-  req.flash('success_msg', '你已經成功登出。')
+  req.flash('success_msg', '已成功登出。')
   res.redirect('/users/login')
 })
 
